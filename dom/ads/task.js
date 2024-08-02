a76f59c7cabcd26c3184ba912663ecb30460531e
Любишов Влад <LubishovV@mik-inform.ru>
2024-08-02 16:51:21 +0300

    change color

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,11 +1,11 @@
 const rotator = document.querySelectorAll('.rotator__case');
 let i = 0;
-
 window.addEventListener('load', () => {
     setInterval(() => {
         rotator[i].classList.remove('rotator__case_active');
         i = (i + 1) % rotator.length;
+        rotator[i].style.color = rotator[i].getAttribute('data-color')
         rotator[i].classList.add('rotator__case_active');
-    }, rotator[i].getAttribute('data-speed'));
+    }, Number(rotator[i].getAttribute('data-speed')));
 });
 
