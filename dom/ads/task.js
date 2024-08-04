804ddf44ec0a7f3d232d1815ef6acb2c82280888
VladLyubishov <lyubishov95@gamil.com>
2024-08-04 17:07:43 +0300

    Выполнено задание 'abs'

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,11 +1,15 @@
 const rotator = document.querySelectorAll('.rotator__case');
 let i = 0;
 window.addEventListener('load', () => {
-    setInterval(() => {
+    changeWord()
+});
+function changeWord(){
+    const interval =  setInterval(() => {
         rotator[i].classList.remove('rotator__case_active');
         i = (i + 1) % rotator.length;
         rotator[i].style.color = rotator[i].getAttribute('data-color')
         rotator[i].classList.add('rotator__case_active');
+        clearInterval(interval);
+        changeWord();
     }, Number(rotator[i].getAttribute('data-speed')));
-});
-
+}
