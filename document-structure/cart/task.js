7d60aa549c36bba462c23f8da2ed9f5cda54ad2e
Любишов Влад <LubishovV@mik-inform.ru>
2024-08-12 12:28:36 +0300

    исправление конфликтов

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 const product = document.querySelectorAll('.product');
 const cartProducts = document.querySelector('.cart__products');
-=======
-const product = document.querySelectorAll('.product')
-const cartProducts = document.querySelector('.cart__products')
-const cartItems = {};
->>>>>>> 3bcd881a0a6709226533bd6c285c5889d2629d11
+
 product.forEach(element => {
     const productInc = element.querySelector('.product__quantity-control_inc');
     const productDec = element.querySelector('.product__quantity-control_dec');
@@ -23,7 +19,6 @@ product.forEach(element => {
     })
 
     productAdd.addEventListener('click', ()=> {
-<<<<<<< HEAD
 
         const productId = element.getAttribute('data-id');
         const cartProductElements = Array.from(cartProducts.querySelectorAll('.cart__product'));
@@ -40,39 +35,6 @@ product.forEach(element => {
                 </div>`
             );
         };
-=======
-        const itemId = element.getAttribute('data-id');
-        if (cartItems[itemId]) {
-            updateElem(Number(productValue.innerText), itemId);
-        } else { 
-            cartItems[itemId] = Number(productValue.innerText);
-            createNewElem(Number(productValue.innerText), itemId, element);
-        }
->>>>>>> 3bcd881a0a6709226533bd6c285c5889d2629d11
-    });
-});
+    })
 
-function updateElem(productValue, itemId) {
-    const cartItem = document.querySelector(`.cart__product[data-id="${itemId}"]`);
-    if (cartItem) {
-      cartItem.querySelector('.cart__product-count').innerText = Number(cartItem.querySelector('.cart__product-count').innerText) + productValue;
-    }
-  }
-
-function createNewElem(productValue, itemId, element){
-    let cartProduct = document.createElement('div');
-    cartProduct.classList.add('cart__product');
-    cartProduct.setAttribute('data-id', itemId);
-  
-    let cartImg = document.createElement('img');
-    cartImg.classList.add('cart__product-image');
-    cartImg.setAttribute('src', element.querySelector('img').getAttribute('src'));
-  
-    let cartCount = document.createElement('div');
-    cartCount.classList.add('cart__product-count');
-    cartCount.innerText = productValue;
-  
-    cartProducts.insertAdjacentElement('afterBegin', cartProduct);
-    cartProduct.insertAdjacentElement('afterBegin', cartImg);
-    cartProduct.insertAdjacentElement('beforeEnd', cartCount);
-}
+})
