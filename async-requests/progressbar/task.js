2181b6848dd112106510f345d96ef85e68f51fef
Любишов Влад <LubishovV@mik-inform.ru>
2024-08-14 16:48:09 +0300

    ProgressBar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,15 @@
+const progress = document.getElementById('progress');
+const form = document.querySelector('#form')
+const xhr = new XMLHttpRequest()
+const formData = new FormData(form)
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    xhr.addEventListener('progress', (event) => {
+        if (event.lengthComputable) {
+            progress.value = event.loaded / event.total
+        }
+    })
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
+    xhr.send(formData)
+})
