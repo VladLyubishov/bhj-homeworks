ec6e5110bf91e7e85f776c16355513427d2c1ef7
Любишов Влад <LubishovV@mik-inform.ru>
2024-08-20 11:11:46 +0300

    popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,9 +1,21 @@
 const modal = document.querySelector('.modal')
 const close = document.querySelector('.modal__close_times')
-modal.classList.add('modal_active');
 
+if (getCookie('modal') != 'active'){
+    modal.classList.add('modal_active');
+    close.addEventListener('click', () => {
+        document.cookie = 'modal=active'
+        modal.classList.remove('modal_active');
+    })
+}
 
-close.addEventListener('click', () => {
-    document.cookie = "moduleClose=active";
-    modal.classList.remove('modal_active');
-})
+function getCookie (name) {
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts
+        .pop()
+        .split(";")
+        .shift();
+    }
+}    
